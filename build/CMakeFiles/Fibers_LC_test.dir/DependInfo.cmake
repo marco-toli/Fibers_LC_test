a05daadc0288d31169b9e3bb8f48e5ae8993b912
Marco Toliman Lucchini <mlucchin@lxplus036.cern.ch>
2016-05-03 19:26:54 +0200

    added compilation for cmake

---
+++ build/CMakeFiles/Fibers_LC_test.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/Fibers_LC_test.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/Fibers_LC_test.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/ConfigFile.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/ConfigFile.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/CreateTree.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/CreateTree.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/DetectorConstruction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/DetectorConstruction.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/EventAction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/EventAction.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/G4EmUserPhysics.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/G4EmUserPhysics.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/MyMaterials.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/MyMaterials.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/PrimaryGeneratorAction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/PrimaryGeneratorAction.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/RunAction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/RunAction.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/SteppingAction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/SteppingAction.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/SteppingVerbose.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/SteppingVerbose.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/TrackInformation.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/TrackInformation.cc.o"
+  "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/src/TrackingAction.cc" "/afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/CMakeFiles/Fibers_LC_test.dir/src/TrackingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/afs/cern.ch/sw/lcg/external/geant4/10.1/x86_64-slc6-gcc49-opt/include/Geant4"
+  "/afs/cern.ch/sw/lcg/external/clhep/2.2.0.4/x86_64-slc6-gcc49-opt/include"
+  "/afs/cern.ch/sw/lcg/external/XercesC/3.1.1p2/x86_64-slc6-gcc47-opt/include"
+  "/afs/cern.ch/sw/lcg/external/qt/4.8.4/x86_64-slc6-gcc47-opt/include"
+  "/afs/cern.ch/sw/lcg/external/qt/4.8.4/x86_64-slc6-gcc47-opt/include/QtCore"
+  "/afs/cern.ch/sw/lcg/external/qt/4.8.4/x86_64-slc6-gcc47-opt/include/QtGui"
+  "/afs/cern.ch/sw/lcg/external/qt/4.8.4/x86_64-slc6-gcc47-opt/include/QtOpenGL"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/Fibers_LC_test_.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Fibers_LC_test_"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/Fibers_LC_test_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
