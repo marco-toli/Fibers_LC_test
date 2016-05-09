20f50ccedce3d7b996b982947c55bd114c09f6dd
Marco Toliman Lucchini <mlucchin@lxplus0056.cern.ch>
2016-05-09 10:19:01 +0200

    vis updates and lancia update

--- template_lancia.sh
+++ template_lancia.sh
@@ -1,1 +1,1 @@
-unbuffer /afs/cern.ch/work/m/mlucchin/Fiber_LC_test/build/Fibers_LC_test /afs/cern.ch/work/m/mlucchin/Fibers_LC_test/Fibers.cfg output_file
+unbuffer /afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/Fibers_LC_test /afs/cern.ch/work/m/mlucchin/Fibers_LC_test/Fibers.cfg gps.mac
