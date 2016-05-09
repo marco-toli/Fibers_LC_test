195f0b82971f731aa25eaf82deb162511da100dd
Marco Toliman Lucchini <mlucchin@lxplus0080.cern.ch>
2016-05-09 18:52:56 +0200

    new myGeant.sh

--- myGeant.sh
+++ myGeant.sh
@@ -1,3 +1,3 @@
 source /afs/cern.ch/sw/lcg/external/geant4/10.1.p01/x86_64-slc6-gcc49-opt/bin/geant4.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.30/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh 
-source /afs/cern.ch/sw/lcg/external/gcc/4.9.2/x86_64-slc6-gcc49-opt/setup.sh
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
