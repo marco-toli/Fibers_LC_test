6062bf574f19d8d56589eb7f1ee559511373db69
Marco Toliman Lucchini <mlucchin@lxplus074.cern.ch>
2016-05-10 14:44:10 +0200

    bux fixing

--- myGeant.sh
+++ myGeant.sh
@@ -1,4 +1,4 @@
 source /afs/cern.ch/sw/lcg/external/geant4/10.1.p01/x86_64-slc6-gcc49-opt/bin/geant4.sh
-source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
-#source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.30/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh 
+#source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.04.02/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.30/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh 
 source /afs/cern.ch/sw/lcg/external/gcc/4.9.3/x86_64-slc6-gcc49-opt/setup.sh
