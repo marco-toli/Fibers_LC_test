d1b59fb449b8a9c9a47e01cbe4808299dd4c52dd
Marco Lucchini <marcot.lucchini@gmail.com>
2017-05-30 12:05:09 +0200

    adding absorber in front of fiber and more tree variables

--- build/CMakeFiles/Fibers_LC_test_.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/Fibers_LC_test_"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/Fibers_LC_test_.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- template_lancia.sh
+++
@@ -1,1 +0,0 @@
-unbuffer /afs/cern.ch/work/m/mlucchin/Fibers_LC_test/build/Fibers_LC_test /afs/cern.ch/work/m/mlucchin/Fibers_LC_test/Fibers.cfg gps.mac
